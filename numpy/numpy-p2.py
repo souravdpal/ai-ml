2af115c71bb5dec865b3b1f5b6754912c43ea665
souravdpal <sourav2026resolution@gmail.com>
2025-08-30 16:28:52 +0530

    numpy part 2 diffrent matrix

---
+++ numpy/numpy-p2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import numpy as np
+
+
+#zero matrix
+zero_matrix =  np.zeros((2,3))
+print(zero_matrix)
+
+#one matrix
+
+one_matrix = np.ones((3,3))
+print(one_matrix)
+
+#full matrix
+full_matrix = np.full((2,9),2)
+print(full_matrix)
+
+#random matrix 
+
+random = np.random.random((4,3))
+print(random)
+
+
+
